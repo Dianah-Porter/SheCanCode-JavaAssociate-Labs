471514d6ec263fc77d22a74e78eea336dbb1f653
Diane Uwamariya <dianahporter@gmail.com>
2026-06-24 00:42:17 +0200

    feat: implemented three observers

---
+++ src/main/java/org/example/lab2_2/order/AuditLogger.java
@@ -0,0 +1,9 @@
+package org.example.lab2_2.order;
+
+public class AuditLogger implements OrderObserver {
+
+    @Override
+    public void onEvent(Order order, OrderEvent event) {
+        System.out.println("AUDIT LOG: " + event + " for order " + order.getId());
+    }
+}
---
+++ src/main/java/org/example/lab2_2/order/EmailNotifier.java
@@ -0,0 +1,9 @@
+package org.example.lab2_2.order;
+
+public class EmailNotifier implements OrderObserver {
+
+    @Override
+    public void onEvent(Order order, OrderEvent event) {
+        System.out.println("EMAIL: Order " + order.getId() + " -> " + event);
+    }
+}
