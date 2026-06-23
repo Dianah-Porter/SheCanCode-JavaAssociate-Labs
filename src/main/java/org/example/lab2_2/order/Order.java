5af302da327f958e7101c52d2186eb5cc8ba0a75
Diane Uwamariya <dianahporter@gmail.com>
2026-06-24 00:39:47 +0200

    feat: order observer interface

--- src/main/java/org/example/lab2_2/order/Order.java
+++ src/main/java/org/example/lab2_2/order/Order.java
@@ -1,4 +1,14 @@
 package org.example.lab2_2.order;
 
 public class Order {
+
+    private final String id;
+
+    public Order(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
 }
